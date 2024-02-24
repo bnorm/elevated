4d9a719bce54da436dbfefdf026048c12e7e7cdd
Brian Norman <brian.norman@jetbrains.com>
2024-02-23 20:19:22 -0600

    Add caching for server static resources

--- service/src/main/kotlin/dev/bnorm/elevated/service/WebConfig.kt
+++ service/src/main/kotlin/dev/bnorm/elevated/service/WebConfig.kt
@@ -1,20 +1,33 @@
 package dev.bnorm.elevated.service
 
 import dev.bnorm.elevated.service.devices.DeviceWebSocketHandler
+import java.net.URI
+import java.util.concurrent.TimeUnit
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.CacheControl
 import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.config.ResourceHandlerRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.coRouter
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.resource.VersionResourceResolver
 import org.springframework.web.reactive.socket.WebSocketHandler
-import java.net.URI
 
 @Configuration
 class WebConfig(
     private val deviceWebSocketHandler: DeviceWebSocketHandler,
-) {
+) : WebFluxConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/ui/**")
+            .addResourceLocations("classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(0, TimeUnit.SECONDS))
+            .resourceChain(true)
+            .addResolver(VersionResourceResolver().addContentVersionStrategy("/**"))
+    }
+
     @Bean
     fun routerFunction() = coRouter {
         GET("/") {
