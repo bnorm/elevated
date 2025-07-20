f66f8e48dccc6c4373e741878057021a36d27f00
Brian Norman <brian.norman@jetbrains.com>
2025-07-20 16:18:11 -0500

    Fix simple window insets for the main screen

--- frontend/android/src/main/kotlin/dev/bnorm/elevated/ui/MainScreen.kt
+++ frontend/android/src/main/kotlin/dev/bnorm/elevated/ui/MainScreen.kt
@@ -1,9 +1,15 @@
 package dev.bnorm.elevated.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import dev.bnorm.elevated.state.auth.UserSession
 import dev.bnorm.elevated.state.auth.UserState
 import dev.bnorm.elevated.ui.screen.HomeScreen
@@ -24,10 +30,16 @@ class MainScreen(
         LaunchedEffect(Unit) { runCatching { userSession.refresh() } }
 
         ElevatedTheme {
-            when (userState) {
-                is UserState.Authenticating -> Unit
-                is UserState.Unauthenticated -> loginScreen.Render()
-                is UserState.Authenticated -> homeScreen.Render()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .windowInsetsPadding(WindowInsets.systemBars)
+            ) {
+                when (userState) {
+                    is UserState.Authenticating -> Unit
+                    is UserState.Unauthenticated -> loginScreen.Render()
+                    is UserState.Authenticated -> homeScreen.Render()
+                }
             }
         }
     }
